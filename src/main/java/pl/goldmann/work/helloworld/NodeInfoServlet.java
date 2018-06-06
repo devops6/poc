d5705c8b5528cf3281c63800335f7d3ccc8320d7
devops6 <39824206+devops6@users.noreply.github.com>
2018-06-06 15:18:11 +0530

    Update NodeInfoServlet.java

--- src/main/java/pl/goldmann/work/helloworld/NodeInfoServlet.java
+++ src/main/java/pl/goldmann/work/helloworld/NodeInfoServlet.java
@@ -15,6 +15,7 @@ public class NodeInfoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         PrintWriter writer = resp.getWriter();
+        writer.println("Testing Only narayan "));
         writer.println("Hostname: " + System.getProperty("jboss.host.name"));
         writer.println("Java Runtime: " + System.getProperty("java.runtime.name") + " " + System.getProperty("java.runtime.version"));
         writer.println("OS: " + System.getProperty("os.name") + " " + System.getProperty("os.arch") + " " + System.getProperty("os.version"));
